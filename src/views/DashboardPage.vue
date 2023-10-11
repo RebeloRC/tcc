2bb42c8068c28ae81a3b84abbde37f53504becf1
Rodrigo Rebelo <rrebelo553@gmail.com>
2023-10-10 23:24:38 -0300

    feat: Product card and Sales analysis card added;

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -4,7 +4,7 @@
     <div>
       <div class="dashboard-content">
         <h1>Inicio</h1>
-        <div>
+        <section class="section-container">
           <div class="card">
             <div class="card-header">
               <div class="card-header-content">
@@ -13,18 +13,55 @@
             </div>
             <div class="card-body">
               <div class="card-body-content">
-                <p>Img</p>
-                <div>
-                  <h4>Heiniken Lata - 350ml</h4>
-                  <div class="">
+                <img src="src/assets/imgs/heineken.png" alt="Heineken" />
+                <div class="card-content-container">
+                  <h3>Heiniken Lata - 350ml</h3>
+                  <div class="card-content-detail">
                     <p>Valor:</p>
-                    <p>R$ 23,25</p>
+                    <b>R$ 23,25</b>
+                  </div>
+                  <div class="card-content-detail">
+                    <p>Vendidos:</p>
+                    <b>480</b>
+                  </div>
+                  <div class="card-content-detail">
+                    <p>Principal método:</p>
+                    <b>Boleto</b>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          <div class="sales-card-container">
+            <h3>Vendas Relacionadas</h3>
+            <div class="sales-card">
+              <div class="sales-card-header">
+                <p>Item</p>
+                <p>(qt.vendas)</p>
+              </div>
+              <div class="sales-card-datail">
+                <p>Fralda</p>
+                <div class="progress-container"></div>
+                <p>50</p>
+              </div>
+              <div class="sales-card-datail">
+                <p>Fralda</p>
+                <div class="progress-container"></div>
+                <p>50</p>
+              </div>
+              <div class="sales-card-datail">
+                <p>Fralda</p>
+                <div class="progress-container"></div>
+                <p>50</p>
+              </div>
+              <div class="sales-card-datail">
+                <p>Fralda</p>
+                <div class="progress-container"></div>
+                <p>50</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   </div>
@@ -48,6 +85,12 @@ export default {
 <style lang="scss">
 .container {
   margin-top: 7.5rem;
+}
+
+.section-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .dashboard-content {
@@ -86,6 +129,72 @@ export default {
         display: flex;
         justify-content: space-between;
         align-items: center;
+
+        .card-content-container {
+          width: 70%;
+          padding: 2.37rem 2.62rem;
+
+          h3 {
+            text-align: center;
+            font-size: 1.62rem;
+          }
+
+          .card-content-detail {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            margin-top: 1.56rem;
+          }
+        }
+      }
+    }
+  }
+
+  .sales-card-container {
+    .sales-card {
+      height: 24.5rem;
+      width: 40rem;
+
+      margin-top: 2.5rem;
+      padding: 20px 46px;
+
+      border: 1px solid #666666;
+      border-radius: 20px;
+
+      background-color: #494949;
+
+      .sales-card-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        font-size: 0.7rem;
+      }
+
+      .sales-card-datail {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+
+        margin-top: 1.56rem;
+
+        .progress-container {
+          width: 80%;
+          height: 0.5rem;
+          border-radius: 0.25rem;
+
+          background-color: #666666;
+
+          &::after {
+            border-radius: 0.25rem;
+            content: '';
+            display: block;
+            width: 50%;
+            height: 100%;
+            background-color: #ffaa05;
+          }
+        }
       }
     }
   }
